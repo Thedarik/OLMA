2372652d538233dc8a53148f361cb9fcf114aea6
The Darik <thedarik2021@gmail.com>
2023-10-26 19:23:47 +0500

    my first commit

---
+++ android/app/src/main/kotlin/com/example/olma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.olma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.brown,
+      ),
+      home: const Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Home Page"),
+      ),
+      body: const Center(
+        child: Text("Hello, Flutter"),
+      ),
+    );
+  }
+}
